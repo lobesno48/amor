bfa805d93de043876902d58da6981cba0e09b2ee
lobesno48 <lobesno48@gmail.com>
2024-11-05 22:32:49 -0300

    Pagina de flores

---
+++ animacion.js
@@ -0,0 +1,27 @@
+// Obtener el botón
+var btnamarilla = document.getElementById("amarilla");
+
+// Agregar el evento de clic al botón
+btnamarilla.onclick = function() {
+    // Crear la ventana emergente con mensaje
+    alert("¡Ventana de flor amarilla!");
+}
+
+// Obtener el botón
+var btnvioleta = document.getElementById("violeta");
+
+// Agregar el evento de clic al botón
+btnvioleta.onclick = function() {
+    // Crear la ventana emergente con mensaje
+    alert("¡Ventana de flor violeta!");
+}
+
+// Obtener el botón
+var btnrosa = document.getElementById("rosa");
+
+// Agregar el evento de clic al botón
+btnrosa.onclick = function() {
+    // Crear la ventana emergente con mensaje
+    alert("¡Ventana de flor rosa!");
+}
+
